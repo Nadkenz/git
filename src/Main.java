34a2c8bed3a70ec5d00b6271a3b8e25694b64db1
NadKenz <nadja.benabla@edu.edugrade.se>
2022-10-13 14:39:56 +0200

    test

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String s = "Detta är mitt första git projekt";
+        System.out.println(s);
+        System.out.println("hej");
+    }
+}
